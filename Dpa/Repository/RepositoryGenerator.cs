96c30b002ab848fb9fb38f2c5de6874fbb91adb5
rudty <rudtyz@gmail.com>
2022-01-08 13:22:25 +0900

    generator 함수별로 분리

--- Dpa/Repository/RepositoryGenerator.cs
+++ Dpa/Repository/RepositoryGenerator.cs
@@ -36,28 +36,105 @@ namespace Dpa.Repository
         }
 
         /// <summary>
+        /// 저장 프로시저 + 직접 구현한 인터페이스
+        /// </summary>
+        /// <typeparam name="Repo">구현체 인터페이스</typeparam>
+        /// <typeparam name="T">entity 타입</typeparam>
+        /// <typeparam name="ID">id 타입</typeparam>
+        /// <param name="dbConnection">연결</param>
+        public static Task<Repo> CustomAndSqlServerStoreProcedure<Repo, T, ID>(DbConnection dbConnection) 
+            where Repo : IStoreProcedureCrudRepository<T, ID>
+        {
+            StoreProcedureRepositoryQuery<T, ID> repositoryQuery = new StoreProcedureRepositoryQuery<T, ID>();
+            Repo repository = (Repo)CustomQueryRepo(
+                dbConnection,
+                typeof(Repo),
+                typeof(DefaultCrudRepository<T, ID>),
+                repositoryQuery);
+            return Task.FromResult(repository);
+        }
+
+        /// <summary>
+        /// 쿼리 + 직접 구현한 인터페이스
+        /// </summary>
+        /// <typeparam name="Repo">구현체 인터페이스</typeparam>
+        /// <typeparam name="T">entity 타입</typeparam>
+        /// <typeparam name="ID">id 타입</typeparam>
+        /// <param name="dbConnection">연결</param>
+        public static Task<Repo> CustomAndTextQuery<Repo, T, ID>(DbConnection dbConnection)
+            where Repo : ICrudRepository<T, ID>
+        {
+            TextRepositoryQuery<T, ID> repositoryQuery = new TextRepositoryQuery<T, ID>();
+            Repo repository = (Repo)CustomQueryRepo(
+                dbConnection,
+                typeof(Repo),
+                typeof(DefaultCrudRepository<T, ID>),
+                repositoryQuery);
+            return Task.FromResult(repository);
+        }
+
+        /// <summary>
         /// 직접 구현한 interface로 사용 가능합니다 
         /// </summary>
         /// <typeparam name="Repo">Repository interface 구현</typeparam>
         /// <param name="dbConnection">연결</param>
         public static Task<Repo> Custom<Repo>(DbConnection dbConnection)
         {
-            Type customType = RuntimeRepositoryGenerator.Generate(typeof(BaseRepository), typeof(Repo));
-            Repo instance = (Repo)Activator.CreateInstance(customType, dbConnection);
-            return Task.FromResult(instance);
+            Type repoType = typeof(Repo);
+            if (!repoType.IsInterface)
+            {
+                throw new ArgumentException("interface only");
+            }
+
+            Type baseType;
+            object repositoryQuery;
+            Type baseImplementRepositoryType;
+            if ((baseImplementRepositoryType = repoType.GetInterface(typeof(IStoreProcedureCrudRepository<,>).Name)) != null) 
+            {
+                baseType = typeof(DefaultCrudRepository<,>).MakeGenericType(baseImplementRepositoryType.GetGenericArguments());
+                Type repositoryQueryType = typeof(StoreProcedureRepositoryQuery<,>)
+                    .MakeGenericType(baseImplementRepositoryType.GetGenericArguments());
+                repositoryQuery = Activator.CreateInstance(repositoryQueryType);
+            }
+            else if ((baseImplementRepositoryType = repoType.GetInterface(typeof(ICrudRepository<,>).Name)) != null)
+            {
+                baseType = typeof(DefaultCrudRepository<,>)
+                    .MakeGenericType(baseImplementRepositoryType.GetGenericArguments());
+                Type repositoryQueryType = typeof(TextRepositoryQuery<,>)
+                    .MakeGenericType(baseImplementRepositoryType.GetGenericArguments());
+                repositoryQuery = Activator.CreateInstance(repositoryQueryType);
+            }
+            else
+            {
+                baseType = typeof(BaseRepository);
+                repositoryQuery = null;
+            }
+            
+            Repo repository = (Repo)CustomQueryRepo(dbConnection, repoType, baseType, repositoryQuery);
+            return Task.FromResult(repository);
         }
 
-        /// <summary>
-        /// 직접 구현한 interface + crud repository로 사용 가능합니다 
-        /// </summary>
-        /// <typeparam name="Repo">Repository interface 구현</typeparam>
-        /// <param name="dbConnection">연결</param>
-        public static Task<Repo> CustomCrud<Repo, T, ID>(DbConnection dbConnection)
+        private static object CustomQueryRepo(DbConnection dbConnection, Type repoType, Type baseType, object repositoryQuery)
         {
-            Type customType = RuntimeRepositoryGenerator.Generate(typeof(DefaultCrudRepository<T, ID>), typeof(Repo));
-            Repo instance = (Repo)Activator.CreateInstance(customType, dbConnection, new TextRepositoryQuery<T, ID>());
-            return Task.FromResult(instance);
+            if (!repoType.IsInterface)
+            {
+                throw new ArgumentException("interface only");
+            }
+
+            Type customType = RuntimeRepositoryGenerator.Generate(baseType, repoType);
+            object[] argument;
+            if (repositoryQuery != null)
+            {
+                argument = new object[] { dbConnection, repositoryQuery };
+            }
+            else
+            {
+                argument = new object[] { dbConnection };
+            }
+            object instance = Activator.CreateInstance(customType, argument, null);
+            return instance;
         }
+
     }
 }
 
